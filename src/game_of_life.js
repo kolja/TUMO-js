e83ea806d858b455b7e32a2112e0f4fb1c43570d
Kolja Wilcke <kolja@apfel.de>
2024-01-25 16:08:55 +0100

    added glow to button (roboter), ported game_of_life to js

---
+++ src/game_of_life.js
@@ -0,0 +1,92 @@
+let cell_size = 16;
+let width = Math.floor(window.innerWidth / cell_size);
+let height = Math.floor(window.innerHeight / cell_size);
+let editmode = true;
+
+function Array2D(rows, columns, init) {
+  return Array.from({ length: rows }, () => Array(columns).fill(init));
+}
+
+function toggle_cell(x, y) {
+  grid[x][y] = grid[x][y] === 0 ? 1 : 0;
+}
+
+function rand_int(r) {
+  return Math.floor(Math.random() * r);
+}
+
+function setup() {
+  grid = Array2D(width, height, 0);
+
+  createCanvas(window.innerWidth, window.innerHeight);
+  frameRate(10);
+  for (i = 0; i < 30; i++) {
+    grid[rand_int(15) + 10][rand_int(15) + 10] = 1;
+  }
+  noStroke();
+  colorMode(HSL, 100);
+}
+
+function drawcube(x, y, state) {
+  let color = 100 * (1 - state);
+  push();
+  translate(cell_size * x, cell_size * y);
+  fill(0, 0, color);
+  rect(0, 0, cell_size, cell_size);
+  pop();
+}
+
+function clamp(value, max) {
+  return (value + max) % max;
+}
+function surrounding(x, y) {
+  let cells = [
+    grid[clamp(x - 1, width)][clamp(y - 1, height)],
+    grid[clamp(x, width)][clamp(y - 1, height)],
+    grid[clamp(x + 1, width)][clamp(y - 1, height)],
+    grid[clamp(x - 1, width)][clamp(y, height)],
+    grid[clamp(x + 1, width)][clamp(y, height)],
+    grid[clamp(x - 1, width)][clamp(y + 1, height)],
+    grid[clamp(x, width)][clamp(y + 1, height)],
+    grid[clamp(x - 1, width)][clamp(y + 1, height)],
+  ];
+  return cells.reduce((x, y) => x + y, 0);
+}
+
+function update_life() {
+  let newGrid = Array2D(width, height, 0);
+  let s = 0;
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      s = surrounding(x, y);
+
+      if (grid[x][y] !== 0 && s < 2) {
+        newGrid[x][y] = 0;
+      } else if (grid[x][y] === 0 && s === 3) {
+        newGrid[x][y] = 1;
+      } else if (grid[x][y] !== 0 && s > 3) {
+        newGrid[x][y] = 0;
+      } else {
+        newGrid[x][y] = grid[x][y];
+      }
+    }
+  }
+  grid = Array.from(newGrid, (row) => Array.from(row));
+}
+
+function draw() {
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      drawcube(x, y, grid[x][y]);
+    }
+  }
+  if (!editmode) update_life();
+}
+
+function mousePressed() {
+  toggle_cell(Math.floor(mouseX / cell_size), Math.floor(mouseY / cell_size));
+}
+
+function keyPressed() {
+  editmode = !editmode;
+}
--- src/roboter.js
+++ src/roboter.js
@@ -12,36 +12,39 @@ function fadenkreuz() {
   pop();
 }
 function button(on) {
-  push()
-  let brightness = on ? 80 : 40;
-  fill(0, 100, brightness)
-  rect(-5, -5, 10, 10)
-  pop()
+  push();
+  let brightness = on ? 90 : 30;
+  let glow = on ? 75 : 40;
+  fill(0, 100, glow, on ? 100 : 0);
+  rect(-8, -8, 16, 16, 4);
+  fill(0, 100, brightness);
+  rect(-5, -5, 10, 10);
+  pop();
 }
 
 function grid(w, h) {
-  for (let y=0; y < h; y++) {
-    for (let x=0; x < w; x++) {
-      push()
-      translate(x*15, y*15)
-      button(Math.random()<0.5)
-      pop()
+  for (let y = 0; y < h; y++) {
+    for (let x = 0; x < w; x++) {
+      push();
+      translate(x * 15, y * 15);
+      button(Math.random() < 0.1);
+      pop();
     }
   }
 }
 
 function arm() {
-  push()
-  fill(0, 0, 60)
-  rect(-20, -20, 40, 150)
-  pop()
+  push();
+  fill(0, 0, 60);
+  rect(-20, -20, 40, 150);
+  pop();
 }
 
 function fuss() {
-  push()
-  fill(0, 0, 60)
-  rect(-30, -30, 60, 40)
-  pop()
+  push();
+  fill(0, 0, 60);
+  rect(-30, -30, 60, 40);
+  pop();
 }
 function kopf() {
   push();
@@ -50,42 +53,42 @@ function kopf() {
   fill(0, 0, 100);
   circle(-50, 0, 50);
   circle(50, 0, 50);
-  fill(0, 100, 30)
-  rect(-20, 30, 40, 7)
+  fill(0, 100, 30);
+  rect(-20, 30, 40, 7);
   pop();
 }
 function bauch() {
   push();
   fill(0, 100, 50);
   rect(-70, -20, 140, 170);
-  translate(-50, 0)
+  translate(-50, 0);
   grid(8, 8);
   pop();
 }
 
 function draw() {
-  push()
+  push();
   fill(15, 45, 80);
-  rect(0, 0, 500, 500)
-  fill(55, 80, 80)
-  rect(0, 0, 500, 200)
-  pop()
+  rect(0, 0, 500, 500);
+  fill(55, 80, 80);
+  rect(0, 0, 500, 200);
+  pop();
 
   translate(250, 100);
 
   kopf();
 
   push();
-  translate(-100, 100)
+  translate(-100, 100);
   arm();
-  translate(200, 0)
+  translate(200, 0);
   arm();
   pop();
 
   push();
-  translate(-40, 280)
+  translate(-40, 280);
   fuss();
-  translate(80, 0)
+  translate(80, 0);
   fuss();
   pop();
 
