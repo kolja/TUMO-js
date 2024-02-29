37784a8f9675e09c97b82ab2af9c38f8f1b6e7b4
Kolja Wilcke <kolja@apfel.de>
2024-02-29 14:00:03 +0100

    fix for workshop2

--- prog2/src/p5intro.js
+++ prog2/src/p5intro.js
@@ -8,6 +8,6 @@ function setup() {
 
 function draw() {
   background(60, 100, 50);
-  fill(random(100), 100, 50);
+  fill(frameCount % 100, 100, 50);
   rect(200, 200, 100, 100); // try replacing some values with mouseX and mouseY...
 }
